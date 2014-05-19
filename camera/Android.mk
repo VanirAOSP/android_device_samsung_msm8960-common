048307acc3eaebf5e274ee32d2c481f098f24f68
nuclearmistake <nuclearmistake@gmail.com>
2014-05-19 08:39:38 -0400

    Fix camera wrapper
    Change-Id: Id5189688539b71111f5999b4728632e08703c58a

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -51,7 +53,9 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.MSM8960
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
